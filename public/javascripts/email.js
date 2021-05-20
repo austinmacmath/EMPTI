369ed98416aa8a0d2cdb36641eba62e4fd247626
Austin Mac <austinmac@ucsb.edu>
2021-05-20 00:50:57 -0700

    add uid check

--- public/javascripts/email.js
+++ public/javascripts/email.js
@@ -3,22 +3,27 @@ console.log('Client-side code running!');
 window.onload = function() {
     var button = document.getElementById('send');
     button.addEventListener('click', function(event) {
+        var str = window.location.pathname;
+        var id = str.substring(
+            str.indexOf("/") + 1, 
+            str.lastIndexOf("/")
+        );
         var response = document.getElementById('email').value;
         fetch('/send', {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({message: response})
+            body: JSON.stringify({message: response, uid: id})
         })
-            .then(function(response) {
-                if(response.ok) {
-                    console.log('Send!');
-                    return;
-                }
-                throw new Error('Send request failed.');
-            })
-            .catch(function(error) {
-                console.log(error);
-            });
+        .then(function(response) {
+            if(response.ok) {
+                console.log('Send!');
+                return;
+            }
+            throw new Error('Send request failed.');
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
         // window.location='/goodbye'
     });
 
@@ -27,16 +32,16 @@ window.onload = function() {
         if(event.key == 'Tab') {
             event.preventDefault();
             fetch('/tab', {method: 'POST'})
-                .then(function(response) {
-                    if(response.ok) {
-                        console.log('Tab!');
-                        return;
-                    }
-                    throw new Error('Tab request failed.');
-                })
-                .catch(function(error) {
-                    console.log(error);
-                });
+            .then(function(response) {
+                if(response.ok) {
+                    console.log('Tab!');
+                    return;
+                }
+                throw new Error('Tab request failed.');
+            })
+            .catch(function(error) {
+                console.log(error);
+            });
         }
     });
 }
--- routes/index.js
+++ routes/index.js
@@ -7,17 +7,37 @@ var router = express.Router();
 router.get('/favicon.ico', (req, res) => res.status(204));
 
 // home page
-router.get('/', function(req, res) {
-  res.render('index')
+router.get('/:uid', function(req, res) {
+  db.one("SELECT COUNT(*) FROM participants WHERE id = $1", [req.params.uid])
+    .then(function (data) {
+      if(data.count == 1) {
+        res.render('index')
+      } else {
+        res.render('wrong_uid')
+      }
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
 });
 
 // emails
-router.get('/email/:promptId', function(req, res) {
-  var d = new Date();
-  var n = d.getHours();
-  db.one('SELECT * FROM email_prompts WHERE id = $1', [req.params.promptId])
+router.get('/:uid/:promptId', function(req, res) {
+  db.one("SELECT COUNT(*) FROM participants WHERE id = $1", [req.params.uid])
     .then(function (data) {
-      res.render('email', { subject: data.description, sender: data.sender, salutation: data.salutation, body: data.body, closing: data.closing, hours: n-9 });
+      if(data.count == 1) {
+        var d = new Date();
+        var n = d.getHours();
+        db.one('SELECT * FROM email_prompts WHERE id = $1', [req.params.promptId])
+          .then(function (data) {
+            res.render('email', { subject: data.description, sender: data.sender, salutation: data.salutation, body: data.body, closing: data.closing, hours: n-9 });
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+      } else {
+        res.render('wrong_uid')
+      }
     })
     .catch(function (error) {
       console.log(error)
@@ -31,9 +51,9 @@ router.get('/goodbye', function(req, res) {
 
 // send button click
 router.post('/send', function (req, res, next) {
-  db.one('INSERT INTO responses(response, submission_time) VALUES ($1, current_timestamp) RETURNING submission_time', [req.body.message])
-    .then(submission_time => {
-      console.log(submission_time)
+  db.one('INSERT INTO responses(response, submission_time, uid) VALUES ($1, current_timestamp, $2) RETURNING uid', [req.body.message, req.body.uid])
+    .then(uid => {
+      console.log(uid)
     })
     .catch(function (error) {
       console.log(error)
