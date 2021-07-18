e21e5781efa6307716b73c95ce6cb776fee49d81
Austin Mac <austinmac@ucsb.edu>
2021-07-18 00:34:43 -0700

    prevent backpaging on tutorial transition

--- public/javascripts/tt0.js
+++ public/javascripts/tt0.js
@@ -1,3 +1,14 @@
+// prevent backpaging
+if (window.history && history.pushState) {
+    addEventListener('load', function() {
+        history.pushState(null, null, null); // creates new history entry with same URL
+        addEventListener('popstate', function() {
+            alert("Please do not return to the previous page.");
+            history.pushState(null, null, null);
+        });    
+    });
+}
+
 window.onload = function() {
     var tutorialSend0 = document.getElementById('tt0');
     if(tutorialSend0 != null) {
