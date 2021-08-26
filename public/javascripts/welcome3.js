499c2e78127e9403e6f0c6b1051509ffea9bdf51
Austin Mac <austinmac@ucsb.edu>
2021-08-25 23:10:50 -0700

    move progresscount

--- public/javascripts/welcome3.js
+++ public/javascripts/welcome3.js
@@ -1,16 +1,14 @@
-var url = window.location.href
-var firstTime = localStorage.getItem(url);
-if(!firstTime) {
-    var progressCount = parseInt(localStorage.getItem("progressCount")) + 1
-    localStorage.setItem("progressCount", progressCount.toString())
-    localStorage.setItem(url, progressCount.toString());
-}
-console.log("Progress Count: " + localStorage.getItem("progressCount"))
-
 window.onload = function () {        
+    var url = window.location.href
+    var firstTime = localStorage.getItem(url);
+    if(!firstTime) {
+        var progressCount = parseInt(localStorage.getItem("progressCount")) + 1
+        localStorage.setItem("progressCount", progressCount.toString())
+        localStorage.setItem(url, progressCount.toString());
+    }
     var progressCount = parseInt(localStorage.getItem(url))
     document.getElementById('completed').style.width = (progressCount * 20).toString() + "px"
-    
+
     var next = document.getElementById('next');
     if (next != null) {
         next.addEventListener('click', function (event) {
