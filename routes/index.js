ab3424147d49072893eb246f3c7aa0c528257e04
Austin Mac <austinmac@ucsb.edu>
2021-09-26 15:55:08 -0700

    s32 alg

--- routes/index.js
+++ routes/index.js
@@ -1118,32 +1118,23 @@ router.post('/s1-6_submit', function (req, res) {
 // submit survey-3-1
 router.post('/s3-1_submit', function (req, res) {
   db.one('INSERT INTO survey_3_1(uid, submission_time, question, answer) VALUES ($1, current_timestamp, $2, $3) RETURNING uid', [req.body.uid, req.body.medium, req.body.frequency])
-    .then(uid => {})
+    .then(uid => {
+      res.sendStatus(200);
+    })
     .catch(function (error) {
       console.log(error)
     })
-  res.sendStatus(200);
 })
 
 // submit survey-3-2
 router.post('/s3-2_submit', function (req, res) {
-  var algorithm
-  db.one("SELECT id, completed, synergy_first, t0_complete, t1_complete FROM participants WHERE id = $1", [req.body.uid])
-    .then(function (data) {
-      if (data.synergy_first && data.t0_complete && data.t1_complete) {
-        algorithm = "CS Predictor"
-      } else if (!data.synergy_first && (!data.t0_complete || !data.t1_complete)) {
-        algorithm = "CS Predictor"
-      } else {
-        algorithm = "Smart Predictor"
-      }
-      return db.one('INSERT INTO survey_3_2(uid, submission_time, algorithm, question, answer) VALUES ($1, current_timestamp, $2, $3, $4) RETURNING uid', [req.body.uid, algorithm, req.body.medium, req.body.frequency])
+  db.one('INSERT INTO survey_3_2(uid, submission_time, question, answer) VALUES ($1, current_timestamp, $2, $3) RETURNING uid', [req.body.uid, req.body.medium, req.body.frequency])
+    .then(uid => {
+      res.sendStatus(200);
     })
-    .then(uid => {})
     .catch(function (error) {
       console.log(error)
     })
-  res.sendStatus(200);
 })
 
 // submit survey-3-3
